2ad48ca021465f08664e9ceb172ba08f1137df5d
Edson Sousa <edsoncarlos33@hotmail.com>
2023-02-24 14:08:41 -0300

    feat: add invoicesDdb resource

--- lib/invoiceWSApi-stack.ts
+++ lib/invoiceWSApi-stack.ts
@@ -14,7 +14,22 @@ export class InvoiceWsApiStack extends cdk.Stack {
     super(scope, id, props)
 
     //Invoice and invoice transaction DDB
-
+    const invoicesDdb = new dynamodb.Table(this, "InvoicesDdb", {
+      tableName: 'invoices',
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+      partitionKey: {
+        name: 'pk',
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: 'sk',
+        type: dynamodb.AttributeType.STRING
+      },
+      timeToLiveAttribute: 'ttl',
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
     //Invoice bucket
 
     //WebSocket connection handler
